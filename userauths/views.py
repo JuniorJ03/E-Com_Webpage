7c098fedbaa0acff2e3dcc8cc69a0e0beb3adf4c
JuniorJ03 <pkj.03.pj@gmail.com>
2025-02-24 15:13:16 -0500

    commit

--- userauths/views.py
+++ userauths/views.py
@@ -4,25 +4,26 @@ from django.contrib.auth import login, authenticate
 from django.contrib import messages
 
 def register_view(request):
-
     if request.method == 'POST':
+       # print("POST request received!")  # Debugging
+       # print(request.POST)  # Check if data is being sent
+        print(request.COOKIES.get('csrftoken'))
         form = UserRegisterForm(request.POST or None)
         if form.is_valid():
             new_user = form.save()
-            username = form.cleaned_data.get('username')   # if you want to you can swap the form.cleaned_data.get('xyz') with just form.cleaned_data['xyz']
-            messages.success(request, f'Hey {username}, your account was created succesfully')
-            new_user = authenticate(username=form.cleaned_data['email'],   # see line 12
-                                    password=form.cleaned_data.get('password1')
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hey {username}, your account was created successfully!')
+
+            new_user = authenticate(
+                request,
+                username=form.cleaned_data['username'],  # If 'email' doesn't work, change to 'username'
+                password=form.cleaned_data.get('password1')
             )
             login(request, new_user)
-            return redirect('core:index')
-   
+            return redirect('core:index')  # Change this if necessary
     else:
         form = UserRegisterForm()
-    
-    context = {
-        'form': form,
-    }
-    print("register_view is being accessed!")
-    return render(request, 'userauths/sign-up.html', context)
 
+    context = {'form': form}
+
+    return render(request, 'userauths/sign-up.html', context)
